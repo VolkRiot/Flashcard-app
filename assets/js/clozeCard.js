c6dd323df9a25f2ab2ea1ad3dcad6f30eb03d8e0
Mikhail Metrikin <metrikin@gmail.com>
2017-04-14 14:03:15 -0700

    Better file system/modulirization

---
+++ assets/js/clozeCard.js
@@ -0,0 +1,26 @@
+function ClozeCard(text, cloze){
+  if(!(this instanceof ClozeCard)){
+    return new ClozeCard(text, cloze);
+  }
+  this.fullText = text;
+  this.cloze = cloze;
+  this.initCard();
+}
+
+ClozeCard.prototype.initCard = function(){
+  var re = new RegExp(this.cloze);
+  if(this.fullText.search(re) == -1){
+    throw new Error("Sorry but it looks like '" + this.cloze + "' is not in '" + this.fullText + "'");
+  }
+
+  this.partial = this.fullText.replace(re, '_'.repeat(this.cloze.length));
+
+  var fs = require('fs');
+  console.log(__dirname);
+  fs.appendFile(__dirname + '/../logs/cards.txt', [this.fullText, this.partial, this.cloze] + "\n", function (err) {
+    if (err) throw err;
+  });
+
+};
+
+module.exports = ClozeCard;
