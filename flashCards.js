99e08bbafb7e27a731f9a2523d5f770b2b2f7cf3
Mikhail Metrikin <metrikin@gmail.com>
2017-06-07 16:22:48 -0700

    Testing heroku presence

--- flashCards.js
+++ flashCards.js
@@ -1,7 +1,10 @@
-var BasicCard = require("./assets/js/basicCard.js");
-var ClozeCard = require("./assets/js/clozeCard");
+var BasicCard = require('./assets/js/basicCard.js');
+var ClozeCard = require('./assets/js/clozeCard');
 
-var firstPresident = BasicCard("Who was the first president of the United States?", "George Washington");
+var firstPresident = BasicCard(
+  'Who was the first president of the United States?',
+  'George Washington',
+);
 
 //"Who was the first president of the United States?"
 console.log(firstPresident.front);
@@ -10,7 +13,9 @@ console.log(firstPresident.front);
 console.log(firstPresident.back);
 
 var firstPresidentCloze = ClozeCard(
-    "George Washington was the first president of the United States.", "George Washington");
+  'George Washington was the first president of the United States.',
+  'George Washington',
+);
 
 // "George Washington"
 console.log(firstPresidentCloze.cloze);
